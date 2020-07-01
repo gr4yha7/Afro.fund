9cdb72b67dbe2922d98bb16287334e70000f0e32
Olusegun Komolafe <joshusegun@gmail.com>
2020-07-01 19:08:56 +0100

    Setup

---
+++ BackendAPI/contracts/AFSCT.sol
@@ -0,0 +1,7 @@
+//Afro.Fund’s Smart Contract Tool is intended to be so intuitive that it follows the Steve Jobs principle: you turn it on and “it just works.”
+
+pragma solidity >=0.4.21 <0.7.0;
+
+contract AFSCT {
+
+}
